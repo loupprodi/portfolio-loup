8917ed5a78b7b77cfb266bb32f481caa6cededd2
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-12-04 16:54:06 -0300

    cleaning code

--- src/mocks/StacksProgram.tsx
+++ src/mocks/StacksProgram.tsx
@@ -13,15 +13,15 @@ import laravel from "../assets/images/laravel-svg.svg"
 import docker from "../assets/images/docker-svg.svg"
 
 export const StacksProgram = [
-    {id:uuidv4(), src:html, alt:'html5'},
-    {id:uuidv4(), src:css, alt:'css3'},
-    {id:uuidv4(), src:js, alt:'js'},
-    {id:uuidv4(), src:react, alt:'react'},
-    {id:uuidv4(), src:node, alt:'node'},
-    {id:uuidv4(), src:sql, alt:'sql'},
-    {id:uuidv4(), src:java, alt:'java'},
-    {id:uuidv4(), src:vue, alt:'vueJs'},
-    {id:uuidv4(), src:php, alt:'php'},
-    {id:uuidv4(), src:laravel , alt:'laravel'},
-    {id:uuidv4(), src:docker, alt:'docker'},
+    {id:uuidv4(), src:html, alt:'html5', class:'html'},
+    {id:uuidv4(), src:css, alt:'css3', class:'css'}, 
+    {id:uuidv4(), src:js, alt:'js', class:'js'},
+    {id:uuidv4(), src:react, alt:'react', class:'react'},
+    {id:uuidv4(), src:node, alt:'node', class:'node'},
+    {id:uuidv4(), src:sql, alt:'sql', class:'sql'},
+    {id:uuidv4(), src:java, alt:'java', class:'java'},
+    {id:uuidv4(), src:vue, alt:'vueJs', class:'vueJs'},
+    {id:uuidv4(), src:php, alt:'php', class:'php'},
+    {id:uuidv4(), src:laravel , alt:'laravel', class:'laravel'},
+    {id:uuidv4(), src:docker, alt:'docker', class:'docker' },
 ]
